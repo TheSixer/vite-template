4f5caa64e127f7a3756d56fd7929f2201b71f248
L.J.Zhang <longjia3713@163.com>
2022-05-13 16:08:00 +0800

    测试项目配置

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{ vitePort }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    const vitePort = ref(import.meta.env.VITE_PORT);
+    return {
+      vitePort,
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
